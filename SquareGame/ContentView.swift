6bcd6fcf7d3219c2bcace755f48f0573bd9173a5
Kavindi12 <kavindifernando1617@gmail.com>
2026-01-10 14:15:54 +0530

    Initial Commit

---
+++ SquareGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SquareGame
+//
+//  Created by COBSCCOMP242P-022 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
